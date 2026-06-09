7da9a616f84b0150cb0da133481dda994ccbbb01
1399640977klx2-beep <1399640977klx2@gmail.com>
2026-06-09 19:17:14 +0800

    Add ui/main_window.py

---
+++ ui/main_window.py
@@ -0,0 +1,66 @@
+"""
+Main window with tabbed interface for OCD Toolbox.
+"""
+from PyQt5.QtWidgets import (
+    QMainWindow, QTabWidget, QMessageBox, QStatusBar, QLabel
+)
+from PyQt5.QtCore import Qt
+
+from ui.tab_ref_aggregator import RefAggregatorTab
+from ui.tab_match import MatchTab
+from ui.tab_wafer_map import WaferMapTab
+from ui.tab_folder_manager import FolderManagerTab
+
+APP_NAME = 'OCD Toolbox'
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(APP_NAME)
+        self.resize(1400, 900)
+
+        self.last_directory = ''
+
+        self._init_ui()
+        self._init_statusbar()
+
+    def _init_ui(self):
+        # Main tab widget
+        self.tabs = QTabWidget()
+        self.tabs.setTabPosition(QTabWidget.North)
+        self.tabs.setDocumentMode(True)
+
+        # Create tabs
+        self.tab_ref = RefAggregatorTab(self)
+        self.tab_match = MatchTab(self)
+        self.tab_map = WaferMapTab(self)
+        self.tab_folder = FolderManagerTab(self)
+
+        self.tabs.addTab(self.tab_ref, 'REF 数据汇总')
+        self.tabs.addTab(self.tab_match, 'Match 匹配')
+        self.tabs.addTab(self.tab_map, 'Wafer 地图')
+        self.tabs.addTab(self.tab_folder, '文件夹管理')
+
+        self.setCentralWidget(self.tabs)
+
+    def _init_statusbar(self):
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
+        self.status_label = QLabel('就绪')
+        self.status_bar.addWidget(self.status_label)
+
+    def set_status(self, message):
+        self.status_label.setText(message)
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(
+            self, '确认退出',
+            '确定要退出 OCD Toolbox 吗？',
+            QMessageBox.Yes | QMessageBox.No,
+            QMessageBox.No
+        )
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
