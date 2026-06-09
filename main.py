4d3706b626849cd59efdcfba05dcf5b733c05be4
1399640977klx2-beep <1399640977klx2@gmail.com>
2026-06-09 19:17:35 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+OCD Toolbox - Main Entry Point
+"""
+import sys
+import os
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+
+# Ensure high-DPI support
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+
+from ui.main_window import MainWindow
+
+APP_NAME = 'OCD Toolbox'
+APP_VERSION = '1.0.0'
+
+
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName(APP_NAME)
+    app.setApplicationVersion(APP_VERSION)
+
+    # Set app style
+    app.setStyle('Fusion')
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
